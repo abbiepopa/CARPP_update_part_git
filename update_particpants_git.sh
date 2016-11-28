389a1f8277eb6f24046215d42a05af5da8ab8348
Abbie Popa <abbiepopa@gmail.com>
2016-11-28 10:51:38 -0800

    initial commit

---
+++ update_particpants_git.sh
@@ -0,0 +1,20 @@
+cd Documents/Lab/UHR\ Tasks/Behavioral\ Scripts/
+cd DT/
+git add DT_Individual_Script.R
+git commit -m "Adding Participant"
+git push origin master
+
+cd ../eDT/
+git add eDT_Individual_Script.R
+git commit -m "Adding Participant"
+git push origin master
+
+cd ../eGNG
+git add eGNG_Individual_Script_post7-23.R
+git commit -m "Adding Participant"
+git push origin master
+
+cd ../GNG/
+git add GNG_Individual_Script_Post7-23.R 
+git commit -m "Adding Participant"
+git push origin master
