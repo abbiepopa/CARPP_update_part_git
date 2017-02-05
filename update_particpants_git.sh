275cc3cd5ec577d5399bc7c1c46f7575a0e729fd
Abbie Popa <abbiepopa@gmail.com>
2017-02-05 10:34:17 -0800

    commenting shell script

--- update_particpants_git.sh
+++ update_particpants_git.sh
@@ -1,19 +1,25 @@
+#set upper level directory (run this script from home)
 cd Documents/Lab/UHR\ Tasks/Behavioral\ Scripts/
+
+#DT
 cd DT/
 git add DT_Individual_Script.R
 git commit -m "Adding Participant"
 git push origin master
 
+#eDT
 cd ../eDT/
 git add eDT_Individual_Script.R
 git commit -m "Adding Participant"
 git push origin master
 
+#eGNG
 cd ../eGNG
 git add eGNG_Individual_Script_post7-23.R
 git commit -m "Adding Participant"
 git push origin master
 
+#GNG
 cd ../GNG/
 git add GNG_Individual_Script_Post7-23.R 
 git commit -m "Adding Participant"
