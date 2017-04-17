c4829f239b95d8c2847fd163005575c0ffd928c7
Abbie Popa <ampopa@ucdavis.edu>
2017-04-17 12:07:17 -0700

    fixed file paths

--- update_particpants_git.sh
+++ update_particpants_git.sh
@@ -1,26 +1,26 @@
 #set upper level directory (run this script from home)
-cd Documents/Lab/UHR\ Tasks/Behavioral\ Scripts/
+cd Documents/carpp/behavior_analysis_scripts/
 
 #DT
-cd DT/
+cd CARPP_DT/
 git add DT_Individual_Script.R
 git commit -m "Adding Participant"
 git push origin master
 
 #eDT
-cd ../eDT/
+cd ../CARPP_eDT/
 git add eDT_Individual_Script.R
 git commit -m "Adding Participant"
 git push origin master
 
 #eGNG
-cd ../eGNG
+cd ../CARPP_eGNG
 git add eGNG_Individual_Script_post7-23.R
 git commit -m "Adding Participant"
 git push origin master
 
 #GNG
-cd ../GNG/
+cd ../CARPP_GNG/
 git add GNG_Individual_Script_Post7-23.R 
 git commit -m "Adding Participant"
 git push origin master
